630db992f64b24b3966524bd0cb4fddd37c75f3d
Moritz Hacker <moritz.e.hacker@uni-bayreuth.de>
2025-11-10 01:09:57 +0100

    [UPDATE] timer ui elements, setting and logic

---
+++ project/js/ui/fields-controls.js
@@ -0,0 +1,38 @@
+var durationInterval;
+
+function startGames() {
+    let rerollBtn = document.getElementById('generate-teams-btn');
+    rerollBtn.classList.add('inactive');
+    rerollBtn.disabled = true;
+    let startBtn = document.getElementById('start-game-btn');
+    startBtn.textContent = "Stop";
+    startBtn.onclick = resetTimer;
+    let duration = parseInt(document.getElementById('timer-input').value);
+    let minutesDisplay = document.getElementById('current-minutes');
+    let secondsDisplay = document.getElementById('current-seconds');
+    let totalSeconds = duration * 60;
+
+    durationInterval = setInterval(() => {
+        minutesDisplay.textContent = Math.floor(totalSeconds / 60).toString().padStart(2, '0');
+        secondsDisplay.textContent = (totalSeconds % 60).toString().padStart(2, '0');
+        if(totalSeconds <= 0) {
+            resetTimer();
+        }
+        totalSeconds--;
+    }, 1000);
+
+}
+
+function resetTimer() {
+    clearInterval(durationInterval);
+    let minutesDisplay = document.getElementById('current-minutes');
+    let secondsDisplay = document.getElementById('current-seconds');
+    let duration = parseInt(document.getElementById('timer-input').value);
+    minutesDisplay.textContent = duration.toString().padStart(2, '0');
+    secondsDisplay.textContent = "00";
+    document.getElementById('start-game-btn').textContent = "Start";
+    let rerollBtn = document.getElementById('generate-teams-btn');
+    rerollBtn.classList.remove('inactive');
+    rerollBtn.disabled = false;
+    document.getElementById('start-game-btn').onclick = startGames;
+}
